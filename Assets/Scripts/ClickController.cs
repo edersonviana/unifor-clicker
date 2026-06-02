413c4bcd73147925e5b195d57a8706a36892cd54
Ederson Viana <edersonvangeli@gmail.com>
2026-06-01 22:40:03 -0300

    add animacao de clique

--- Assets/Scripts/ClickController.cs
+++ Assets/Scripts/ClickController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -5,26 +6,63 @@ using UnityEngine.EventSystems;
 public class ClickController : MonoBehaviour, IPointerClickHandler
 {
     public AudioClip clickSound;
+
+    [Header("Animação de Clique")]
+    public float escalaMinima = 0.97f;
+    public float duracaoAnimacao = 0.15f;
+
     private AudioSource audioSource;
+    private RectTransform rectTransform;
+    private Vector3 escalaOriginal;
+    private Coroutine animacaoAtual;
 
     void Awake()
     {
         audioSource = GetComponent<AudioSource>();
         if (audioSource != null)
-        {
             audioSource.playOnAwake = false;
-        }
+
+        rectTransform = GetComponent<RectTransform>();
+        escalaOriginal = rectTransform.localScale;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("Clicou!");
-        
         if (audioSource != null && clickSound != null)
-        {
             audioSource.PlayOneShot(clickSound);
-        }
 
         GameManager.Instance.Clicar();
+
+        if (animacaoAtual != null)
+            StopCoroutine(animacaoAtual);
+        animacaoAtual = StartCoroutine(AnimarClique());
+    }
+
+    private IEnumerator AnimarClique()
+    {
+        float metade = duracaoAnimacao / 2f;
+
+        // Encolhe
+        float t = 0f;
+        while (t < metade)
+        {
+            t += Time.deltaTime;
+            float fator = Mathf.Lerp(1f, escalaMinima, t / metade);
+            rectTransform.localScale = escalaOriginal * fator;
+            yield return null;
+        }
+
+        // Volta ao normal
+        t = 0f;
+        while (t < metade)
+        {
+            t += Time.deltaTime;
+            float fator = Mathf.Lerp(escalaMinima, 1f, t / metade);
+            rectTransform.localScale = escalaOriginal * fator;
+            yield return null;
+        }
+
+        rectTransform.localScale = escalaOriginal;
+        animacaoAtual = null;
     }
 }
